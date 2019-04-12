30bd5da646cd6aea4b611de3935dd4735d0f02c7
Fedor Skripnikov <s-fyodor@yandex.ru>
2019-04-12 20:06:36 +0300

    Minor fixes

--- src/main/java/com/mycompany/wheretogo/service/UserServiceImpl.java
+++ src/main/java/com/mycompany/wheretogo/service/UserServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
